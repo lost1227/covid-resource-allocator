770add9edcb22dea2946d77d8f0ef76d53abe60f
Jordan Powers <jordanpowers1227@gmail.com>
2020-04-22 20:48:57 -0700

    Add spring and angular projects

---
+++ spring/src/main/java/edu/calpoly/csc_308/cora/CoraApplication.java
@@ -0,0 +1,13 @@
+package edu.calpoly.csc_308.cora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoraApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/edu/calpoly/csc_308/cora/CoraApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.calpoly.csc_308.cora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
