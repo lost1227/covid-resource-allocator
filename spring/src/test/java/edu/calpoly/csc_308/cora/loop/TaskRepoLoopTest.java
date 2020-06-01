31a7d6a83b48857476f9026c5bf2442bd1d5a987
Jordan Powers <jordanpowers1227@gmail.com>
2020-06-01 13:52:59 -0700

    Fix merge issues

--- spring/src/test/java/edu/calpoly/csc_308/cora/loop/TaskRepoLoopTest.java
+++ spring/src/test/java/edu/calpoly/csc_308/cora/loop/TaskRepoLoopTest.java
@@ -37,19 +37,25 @@ public class TaskRepoLoopTest {
     repo.deleteAll();
 
     tasks.add(new VolunteerTaskDAO(
-      "Supply Collector",
-      "Long Beach, CA",
+      new VolunteerTaskDAO.TaskProfile(
+        "Supply Collector",
+        "Long Beach, CA",
+        "Volunteers are needed to assist in the collection of donated supplies for distribution to medical facilities.",
+        "No instructions."
+      ),
       1,
-      "Volunteers are needed to assist in the collection of donated supplies for distribution to medical facilities.",
       3L,
       "",
       -1L
     ));
     tasks.add( new VolunteerTaskDAO(
-      "Pamphlet Designer",
-      "Long Beach, CA",
+      new VolunteerTaskDAO.TaskProfile(
+        "Pamphlet Designer",
+        "Long Beach, CA",
+        "A graphic designer is needed to assist in the creation of informational brocures and pamphlets that will help inform the community on how to stay safe during the COVID pandemic.",
+        "Message me for more instructions."
+      ),
       0,
-      "A graphic designer is needed to assist in the creation of informational brocures and pamphlets that will help inform the community on how to stay safe during the COVID pandemic.",
       4L,
       "Graphic Design",
       -1L
