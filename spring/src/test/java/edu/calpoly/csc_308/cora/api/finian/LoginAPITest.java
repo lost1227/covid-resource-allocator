1b4b46a3bed44f7d2dee882928bec59bc5cf2530
frawson10 <finrawson@gmail.com>
2020-06-04 17:34:33 -0700

    second class

--- spring/src/test/java/edu/calpoly/csc_308/cora/api/finian/LoginAPITest.java
+++ spring/src/test/java/edu/calpoly/csc_308/cora/api/finian/LoginAPITest.java
@@ -4,73 +4,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.core.Is.is;
-import org.hamcrest.collection.IsEmptyCollection;
-import static org.hamcrest.CoreMatchers.*;
-
-import java.util.Collection;
-import java.util.List;
 
 import edu.calpoly.csc_308.cora.data.users.UserDAO;
 import edu.calpoly.csc_308.cora.data.users.UserRepository;
 import edu.calpoly.csc_308.cora.entities.User;
 import edu.calpoly.csc_308.cora.services.UserManager;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import edu.calpoly.csc_308.cora.api.MessengerAPI;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import org.springframework.http.ResponseEntity;
-
 import org.mockito.Mockito;
 
-import edu.calpoly.csc_308.cora.api.request.SendMessageRequestModel;
-import edu.calpoly.csc_308.cora.api.response.ListConversationsResponse;
-import edu.calpoly.csc_308.cora.api.response.SuccessResponse;
-import edu.calpoly.csc_308.cora.data.users.UserDAO;
-import edu.calpoly.csc_308.cora.data.users.UserRepository;
-import edu.calpoly.csc_308.cora.entities.Conversation;
-import edu.calpoly.csc_308.cora.services.Messenger;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import org.springframework.security.core.Authentication;
-
-import edu.calpoly.csc_308.cora.api.request.EditUserRequestModel;
-import edu.calpoly.csc_308.cora.api.request.NewUserRequestModel;
-import edu.calpoly.csc_308.cora.api.response.ResponseModel;
-import edu.calpoly.csc_308.cora.api.response.SuccessResponse;
 import edu.calpoly.csc_308.cora.api.response.UserInfoResponse;
-import edu.calpoly.csc_308.cora.entities.User;
-import edu.calpoly.csc_308.cora.security.AuthUser;
-import edu.calpoly.csc_308.cora.security.AuthUserService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class LoginAPITest {
